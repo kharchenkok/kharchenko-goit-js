f8b3c53cce51d64ed591984171540467bb3d89e9
kharchenkok <k.v.kharchenko@ukr.net>
2020-06-20 23:15:31 +0300

    ht03 added

--- module03/index03.js
+++ module03/index03.js
@@ -29,14 +29,70 @@
 // }
 
 // ===============home-task-02=================================================
-const countProps = function(obj) {
-    // const keys=Object.keys(obj);
-    return Object.keys(obj).length
+// const countProps = function(obj) {
+//     // const keys=Object.keys(obj);
+//     return Object.keys(obj).length
     
+// };
+  
+//   console.log(countProps({})); // 0
+  
+//   console.log(countProps({ name: 'Mango', age: 2 })); // 2
+  
+//   console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+
+
+
+// ===================home-task-03=================================================
+
+const findBestEmployee = function(employees) {
+    let bestResult =0;
+    for(let workResult of Object.values(employees)){
+        if(workResult>bestResult){
+            bestResult=workResult
+        }
+    }
+    return bestResult
 };
+console.log(employees)
+
+
+// const findBestEmployee = function(employees) {
+//         let bestResult =0;
+//         let nameBestEmpl;
+//         for(let key in employees){
+//             if(employees[key]>bestResult){
+//                 bestResult=employees[key];
+//                 nameBestEmpl=key;
+//             }
+//         }
+//         return nameBestEmpl
+    
+//       };
   
-  console.log(countProps({})); // 0
+
+  console.log(
+    findBestEmployee({
+      ann: 29,
+      david: 35,
+      helen: 1,
+      lorence: 99,
+    }),
+  ); // lorence
   
-  console.log(countProps({ name: 'Mango', age: 2 })); // 2
+  console.log(
+    findBestEmployee({
+      poly: 12,
+      mango: 17,
+      ajax: 4,
+    }),
+  ); // mango
   
-  console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+  console.log(
+    findBestEmployee({
+      lux: 147,
+      david: 21,
+      kiwi: 19,
+      chelsy: 38,
+    }),
+  ); // lux
