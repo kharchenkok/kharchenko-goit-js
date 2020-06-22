aaeaca8bd7f15723b91db8c2b8028ce9137c76de
kharchenkok <k.v.kharchenko@ukr.net>
2020-06-22 23:42:05 +0300

    control commit

--- module02/index02.js
+++ module02/index02.js
@@ -133,3 +133,42 @@ logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
         
 //     } while(input !==null);
 //     console.log(`Загальна сума чисел ${total}`);
+
+// ==================home-task-7============================
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+// const isLoginValid = function(login) {
+//         if(login.length>=4 && login.length<=16){
+//         return true;
+//       }
+//       return false;
+
+// };
+
+// const isLoginUnique = function(allLogins, login) {
+//   if(allLogins.includes(login)){
+//       return false;
+//   }
+//   return true;
+// };
+
+// const addLogin = function(allLogins, login) {
+//     if(isLoginValid(login) && isLoginUnique(allLogins, login)){
+//         allLogins.push(login);
+//         console.log(allLogins);
+        
+//         return 'Логин успешно добавлен!';
+//     }else if(!isLoginValid(login)){
+//         return 'Ошибка! Логин должен быть от 4 до 16 символов'
+//     }else if(!isLoginUnique(allLogins, login)){
+//         return 'Такой логин уже используется!'
+//     }
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+// console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+// console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+// console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
