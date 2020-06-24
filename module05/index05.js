7e04a884a20db806ce0216f1ccbfb8f279cd65d8
kharchenkok <k.v.kharchenko@ukr.net>
2020-06-24 23:23:23 +0300

    home-task-03 added

--- module05/index05.js
+++ module05/index05.js
@@ -31,40 +31,95 @@
 // poly.getInfo(); // Login: Poly, Email: poly@mail.com
 
 // ========================home-task-02==============================
-// Напиши класс User для создания пользователя со следующим свойствами:
+// // Напиши класс User для создания пользователя со следующим свойствами:
 
-// name - строка
-// age - число
-// followers - число
-// Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
+// // name - строка
+// // age - число
+// // followers - число
+// // Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
 
-class User{
-    constructor(name,age,followers){
-        this.name=name;
-        this.age=age;
-        this.followers=followers
+// class User{
+//     constructor(name,age,followers){
+//         this.name=name;
+//         this.age=age;
+//         this.followers=followers
+//     }
+//     getInfo(){
+//         return console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
+//     }
+// }
+
+// const mango = new User('Mango',2,20);
+// console.table(mango)
+// // const mango = new User({
+//     //   name: 'Mango',
+//     //   age: 2,
+//     //   followers: 20,
+//     // });
+    
+//     mango.getInfo(); // User Mango is 2 years old and has 20 followers
+    
+// const poly = new User('Poly',3,17);
+// console.table(poly)
+// // const poly = new User({
+// //   name: 'Poly',
+// //   age: 3,
+// //   followers: 17,
+// // });
+
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+// ==================================home-task-03=========================================
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров. 
+// При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+class Storage{
+    constructor(arrayProd){
+        this.items=arrayProd
     }
-    getInfo(){
-        return console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
+    getItems(){
+        return this.items
+
     }
+    addItem(item){
+        return this.items.push(item)
+    }
+    removeItem(item){
+
+        if(this.items.includes(item)){
+            this.items.splice(this.items.indexOf(item),1)
+        }
+        
+        return this.items
+        
+    }
+    
 }
 
-const mango = new User('Mango',2,20);
-console.table(mango)
-// const mango = new User({
-    //   name: 'Mango',
-    //   age: 2,
-    //   followers: 20,
-    // });
-    
-    mango.getInfo(); // User Mango is 2 years old and has 20 followers
-    
-const poly = new User('Poly',3,17);
-console.table(poly)
-// const poly = new User({
-//   name: 'Poly',
-//   age: 3,
-//   followers: 17,
-// });
 
-poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+
+
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+console.log(storage);
+console.log(storage.items.indexOf('Пролонгер'));
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
