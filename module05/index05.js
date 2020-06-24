50e64c3c44a930190a3fcbf193edb87fc2ad7874
kharchenkok <k.v.kharchenko@ukr.net>
2020-06-24 21:47:47 +0300

    home-task-01-02 added

--- module05/index05.js
+++ module05/index05.js
@@ -1,1 +1,70 @@
+// =================home-task-01===========================================
+// Напиши функцию-конструктор Account, которая создает объект со свойствами login и email. 
+// В prototype функции-конструктора добавь метод getInfo(), который выводит в консоль значения 
+// полей login и email объекта который его вызвал.
 
+// const Account=function(login,email){
+//     this.login=login;
+//     this.email=email;
+// }
+
+// Account.prototype.getInfo=function(){
+//     return console.log(`Login: ${this.login}, Email: ${this.email}`)
+// }
+
+// console.log(Account.prototype.getInfo); // function
+
+// const mango = new Account('Mangozedog','mango@dog.woof')
+// console.log(mango);
+// // const mango = new Account({
+// //   login: 'Mangozedog',
+// //   email: 'mango@dog.woof',
+// // });
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+    
+// const poly = new Account('Poly','poly@mail.com');
+// console.log(poly);
+// // const poly = new Account({
+// //   login: 'Poly',
+// //   email: 'poly@mail.com',
+// // });
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+// ========================home-task-02==============================
+// Напиши класс User для создания пользователя со следующим свойствами:
+
+// name - строка
+// age - число
+// followers - число
+// Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
+
+class User{
+    constructor(name,age,followers){
+        this.name=name;
+        this.age=age;
+        this.followers=followers
+    }
+    getInfo(){
+        return console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
+    }
+}
+
+const mango = new User('Mango',2,20);
+console.table(mango)
+// const mango = new User({
+    //   name: 'Mango',
+    //   age: 2,
+    //   followers: 20,
+    // });
+    
+    mango.getInfo(); // User Mango is 2 years old and has 20 followers
+    
+const poly = new User('Poly',3,17);
+console.table(poly)
+// const poly = new User({
+//   name: 'Poly',
+//   age: 3,
+//   followers: 17,
+// });
+
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
