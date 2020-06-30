1feb535bd8c2dfcd223056451958caaf4a7cc44e
kharchenkok <k.v.kharchenko@ukr.net>
2020-06-30 18:48:42 +0300

    task01 added

--- module07/task01/index07_01.js
+++ module07/task01/index07_01.js
@@ -1,64 +1,48 @@
-// =========================home-task-01==================================
-
 // Напиши скрипт, который выполнит следующие операции.
 
-// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+//  Получится 'В списке 3 категории.'.
 
-const itemList=document.querySelector('#categories');
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в 
+// консоль текст заголовка элемента (тега h2) и количество элементов в категории
+//  (всех вложенных в него элементов li).
+
+// Например для первой категории получится:
+
+// Категория: Животные
+// Количество элементов: 4
+
+const itemList = document.querySelector('#categories')
 // console.log(itemList);
-// console.dir(itemList);
-// const numbOfItem=itemList.children.length;
-// console.log(`В списке ${numbOfItem} категории`)
+// console.dir(itemList.querySelectorAll('.item'));
+// console.dir(itemList.querySelectorAll('h2'));
+// console.dir(itemList.children);
+
+console.log(`В списке ${itemList.children.length} категории`);
+
+// ===================ex1==============================================
+const itemArr=document.querySelectorAll('.item')
+// // console.log(itemArr);
 
 
-// // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента 
-// // (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
-
-// // Например для первой категории получится:
-
-// // Категория: Животные
-// // Количество элементов: 4
-
-const arrLi = itemList.querySelectorAll('.item');
-// const arrH2 = itemList.querySelector('h2').textContent;
-console.log(arrLi);
-
-
-// // const arrH2= document.querySelectorAll('h2')
-// // console.log(arrH2);
-
-// // console.log(arrLi);
-// const newArrLi = Array.from(arrLi)
-// console.log(newArrLi);
-// console.log(itemList.querySelectorAll('h2'))
-// // for(let i=0; i<arrLi.length; i+=1){
-// //     const category = arrLi[i].children[0].textContent;
-// //     console.log(category);
-// //     const categoryList = arrLi[i].children[1].children.length;
-// //     console.log(categoryList);
+// for(let i=0; i<itemArr.length; i+=1){
+//         const category = itemArr[i].children[0].textContent;
+//         console.log(`Категория: ${category}`);
+//         const categoryList = itemArr[i].children[1].children.length;
+//         console.log(`Количество элементов: ${categoryList}`);
+        
+//     }
+for(let i=0; i<itemArr.length; i+=1){
+            const category = itemArr[i].querySelector('h2').textContent;
+            console.log(`Категория: ${category}`);
+            const categoryList = itemArr[i].querySelector('ul').children.length;
+            console.log(`Количество элементов: ${categoryList}`);
+            
+        }
+// ===================ex2================================================
+// itemArr.forEach(element => {
     
-// // }
-// arrLi.forEach(elem => 
-//     console.log(itemList.querySelector('h2').textContent)
-// );
-
-
-// const categories = document.querySelector("#categories");
-
-arrLi.forEach((element) => {
-  console.log(`Категория: ${element.querySelector("h2").textContent}`);
-  console.log(
-    `Количество элементов: ${element.querySelector("ul").children.length}`
-  );
-});
-
-
-
-
-
-
-
-
-
-
-// =====================home-task-02=========================================
+//     console.log(`Категория:${element.querySelector('h2').textContent}`);
+//     console.log(`Количество элементов:${element.querySelector('ul').children.length}`);
+    
+// })
