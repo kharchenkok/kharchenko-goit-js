96a63f2930984ebb064cdf319a138e41c1968d83
kharchenkok <k.v.kharchenko@ukr.net>
2020-07-10 22:28:58 +0300

    chngs saved

--- module10/src/index.js
+++ module10/src/index.js
@@ -2,16 +2,16 @@ import menuItems from './menu.json';
 import menuItemsTemplate from './menu-items.hbs';
 import './styles.css';
 import './js/themeChange';
-import './js/pagination';
+// import './js/pagination';
 
-// console.log(menuItems.length);
-// console.log(menuItemsTemplate(menuItems));
+const munuItemsJS ={
+    menuItems,
+    
+}
+
 const menu =document.querySelector('.js-menu')
 
-
-
 let markup = menuItemsTemplate(menuItems);
-
 
 menu.insertAdjacentHTML('afterbegin', markup);
 
